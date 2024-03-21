ee864b012eac334e40a1f1d074faddbfd7e9eaee
Lala-pukuchan <ruruover1105@gmail.com>
2024-03-21 10:19:30 +0100

    ex00

--- ex00/S1E9.py
+++ ex00/S1E9.py
@@ -5,24 +5,19 @@ class Character(ABC):
     """Docstring for class Character"""
 
     @abstractmethod
-    def __init__(self, name, is_alive=True):
+    def __init__(self, first_name, is_alive=True):
         """Docstring for Constructor of class Character"""
-        self.name = name
+        self.first_name = first_name
         self.is_alive = is_alive
 
-    @abstractmethod
     def die(self):
         """Docstring for method die of class Character"""
-        pass
+        self.is_alive = False
 
 
 class Stark(Character):
     """Docstring for class Stark"""
 
-    def __init__(self, name, is_alive=True):
+    def __init__(self, first_name, is_alive=True):
         """Docstring for Constructor of class Stark"""
-        super().__init__(name, is_alive)
-
-    def die(self):
-        """Docstring for method die of class Stark"""
-        self.is_alive = False
+        super().__init__(first_name, is_alive)
